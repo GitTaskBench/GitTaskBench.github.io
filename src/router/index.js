e1419aaa6368e76c8e32e3c23727c1d2f6f943c0
xiangshu1896 <apple@appledeMacBook-Pro.local>
2025-09-12 22:42:00 +0400

    0912: optimization7

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/Home.vue'
 import LeaderBoard from '../views/LeaderBoard.vue'
 
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/GitTaskBench.github.io/' : '/'),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/GitTaskBench.github.io/' : '/',
+  publicPath: './',
   devServer: {
     port: 8080,
     open: true
